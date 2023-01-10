770290565f2284baa7e06104cd4dc3aa7a67490a
EnyaHong <enya.hong@kellogg.northwestern.edu>
2023-01-10 22:24:43 +0000

    Class 2 in-class practices

--- lab-3-2.sql
+++ lab-3-2.sql
@@ -1,5 +1,10 @@
 -- For each team, what is the average number of wins per season, in the 21st century?
 
+SELECT name, AVG (wins)
+FROM teams
+WHERE year > 2000
+GROUP By name;
+
 -- Expected result:
 --
 -- +-------------------------------+------------------+
